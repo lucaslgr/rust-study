54b4e36ec865c3fbcc17961513281416890b5d34
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-06-07 21:26:01 -0300

    Remove target folder

--- course-udemy/http-server/target/rls/debug/deps/http_server-92f535d3c2b86597.d
+++
@@ -1,5 +0,0 @@
-/home/guimaraes/projects/rust/course-udemy/http-server/target/rls/debug/deps/http_server-92f535d3c2b86597.rmeta: src/main.rs
-
-/home/guimaraes/projects/rust/course-udemy/http-server/target/rls/debug/deps/http_server-92f535d3c2b86597.d: src/main.rs
-
-src/main.rs:
--- course-udemy/http-server/target/rls/debug/deps/http_server-c72700f8f821b162.d
+++
@@ -1,5 +0,0 @@
-/home/guimaraes/projects/rust/course-udemy/http-server/target/rls/debug/deps/http_server-c72700f8f821b162.rmeta: src/main.rs
-
-/home/guimaraes/projects/rust/course-udemy/http-server/target/rls/debug/deps/http_server-c72700f8f821b162.d: src/main.rs
-
-src/main.rs:
