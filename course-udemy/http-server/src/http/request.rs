6947b83309b29530a92e5a534e55abc91d220941
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-02 23:50:40 -0300

    Parsing values from Strings and 'If Let' expression

--- course-udemy/http-server/src/http/request.rs
+++ course-udemy/http-server/src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
@@ -36,12 +36,22 @@ impl TryFrom<&[u8]> for Request {
     }
 
     let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-    let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
     let (protocol, _) =  get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
     if protocol != "HTTP/1.1" {
       return Err(ParseError::InvalidProtocol);
     }
+
+    let method: Method= method.parse()?;
+
+    let mut query_string = None;
+    
+    if let Some(i) = path.find('?') {
+      query_string = Some(&path[i+1..]);
+      path = &path[..i];
+    }
+
 
     unimplemented!()
   }
@@ -71,6 +81,12 @@ impl ParseError {
       Self::InvalidProtocol => "Invalid Protocol",
       Self::InvalidMethodError => "Invalid MethodError",
     }
+  }
+}
+
+impl From<MethodError> for ParseError {
+  fn from(_: MethodError) -> Self {
+    Self::InvalidMethodError
   }
 }
 
