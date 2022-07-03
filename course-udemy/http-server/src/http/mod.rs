abc3842c69cbaff5b094fe0b1600024cb18d88c6
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-03 14:04:58 -0300

    Implements the response struct

--- course-udemy/http-server/src/http/mod.rs
+++ course-udemy/http-server/src/http/mod.rs
@@ -1,7 +1,9 @@
 pub use method::Method;
 pub use request::{Request, ParseError};
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
---
+++ course-udemy/http-server/src/http/response.rs
@@ -0,0 +1,14 @@
+pub enum StatusCode {
+
+}
+
+pub struct Response {
+  status_code: StatusCode,
+  body: Option<String>
+}
+
+impl Response {
+  pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+    Self { status_code, body }
+  } 
+}
