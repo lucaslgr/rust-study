2071a0409332e4849de164d226f0c94effc7044e
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-02 22:36:11 -0300

    Removing temp files

--- course-udemy/http-server/src/http/request.rs
+++ course-udemy/http-server/src/http/request.rs
@@ -1,7 +1,95 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
   path: String,
   query_string: Option<String>,
   method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+  type Error = ParseError;
+
+  fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+  //   match str::from_utf8(buf) {
+  //     Ok(request) => {},
+  //     Err(_) => return Err(ParseError::InvalidEncoding),
+  //   }
+
+  //   match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+  //     Ok(request) => {},
+  //     Err(e) => return Err(e)
+  //   }
+
+  // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+    //After to implement the From trait to ParseError we can do this straight way below:
+    let request = str::from_utf8(buf)?;
+
+    match get_next_word(request) {
+      Some((method, request)) => {},
+      None => return Err(ParseError::InvalidRequest),
+    }
+
+    let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (protocol, _) =  get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+    if protocol != "HTTP/1.1" {
+      return Err(ParseError::InvalidProtocol);
+    }
+
+    unimplemented!()
+  }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)>{
+  for (i, val) in request.chars().enumerate() {
+    if val == ' ' || val == '\r' || val == '\n' {
+      return Some((&request[..i], &request[i+1..]));
+    }
+  }
+  None
+}
+
+pub enum ParseError {
+  InvalidRequest,
+  InvalidEncoding,
+  InvalidProtocol,
+  InvalidMethodError,
+}
+
+impl ParseError {
+  fn message(&self) -> &str {
+    match self {
+      Self::InvalidRequest => "Invalid Request",
+      Self::InvalidEncoding => "Invalid Encoding",
+      Self::InvalidProtocol => "Invalid Protocol",
+      Self::InvalidMethodError => "Invalid MethodError",
+    }
+  }
+}
+
+impl From<Utf8Error> for ParseError {
+  fn from(_: Utf8Error) -> Self {
+    Self::InvalidEncoding
+  }
+}
+
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl Error for ParseError {}
--- course-udemy/http-server/src/server.rs
+++ course-udemy/http-server/src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -20,8 +22,19 @@ impl Server {
         Ok((mut tcp_stream, socket_addr)) => {
           println!("Receive a package from client_addres: {}", socket_addr);
 
-          let mut buffer: [u8] = [0; 1024];
-          tcp_stream.read(&mut buffer);
+          let mut buffer: [u8; 1024] = [0; 1024];
+          match tcp_stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+              match Request::try_from(&buffer[..]) {
+                Ok(request) => {}
+                Err(e) => println!("Failed to convert the buffer into a request {}", e),
+              }
+            }
+            Err(e) => {
+              println!("Failed to establish a connection: {}", e);
+            }
+          }
         }
         Err(error) => {
           println!("Failed to establish a connection: {} ", error);
