00b3ac924e453912098e11b6933225ef22a61881
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-04 09:19:12 -0300

    Working with Environment Variables

--- course-udemy/http-server/src/main.rs
+++ course-udemy/http-server/src/main.rs
@@ -1,4 +1,4 @@
-use http::Method;
+use std::env;
 use server::Server;
 use website_handler::WebSiteHandler;
 
@@ -7,8 +7,8 @@ mod server;
 mod website_handler;
 
 fn main() {
-    let get = Method::GET;
-
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebSiteHandler);
+  let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+  let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+  let server = Server::new("127.0.0.1:8080".to_string());
+  server.run(WebSiteHandler::new(public_path));
 }
