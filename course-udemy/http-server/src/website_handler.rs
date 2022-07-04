4d2b48272c8f9a5c961013f8b0ede12b16e4f65c
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-04 09:02:13 -0300

    Routing incomming requests

--- course-udemy/http-server/src/website_handler.rs
+++ course-udemy/http-server/src/website_handler.rs
@@ -1,10 +1,17 @@
 use super::server::Handler;
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Request, Response, StatusCode, Method};
 
 pub struct  WebSiteHandler;
 
 impl Handler for WebSiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
-    Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+    match request.method() {
+      _ => Response::new(StatusCode::NotFound, None),
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+        "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      }
+    }
   }
 }
