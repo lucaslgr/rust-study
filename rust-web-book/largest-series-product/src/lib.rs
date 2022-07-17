907e68f36381be27e7d37ac8b08cc8c4a898a99c
Guimaraes <lucas.guimaraes@ifood.com.br>
2022-07-17 10:31:58 -0300

    Finishes the largest series products exercise

--- rust-web-book/largest-series-product/src/lib.rs
+++ rust-web-book/largest-series-product/src/lib.rs
@@ -4,6 +4,50 @@ pub enum Error {
     InvalidDigit(char),
 }
 
-pub fn lsp(_: &str, _: usize) -> Result<u64, Error> {
-    Ok(18u64)
+pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    let v_alphanumeric = string_digits.matches(char::is_alphabetic).collect::<Vec<&str>>();
+    
+    if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    }
+    else if span == 0 {
+        return Ok(1u64);
+    }
+    else if v_alphanumeric.len() > 0 {
+        return Err(Error::InvalidDigit(
+            v_alphanumeric
+            .first().unwrap()
+            .to_owned()
+            .to_string()
+            .pop()
+            .unwrap()
+        ));
+    }
+
+    let mut digits: Vec<u64> = string_digits
+        .split("")
+        .map(|digit| digit.parse())
+        .filter(|digit| match digit {
+            Ok(_) => true,
+            Err(_) => false,
+        })
+        .map(|d| d.unwrap())
+        .collect();
+    digits.reverse();
+    Ok(window_values(digits, span)
+        .first()
+        .unwrap()
+        .to_owned()
+    )
 }
+
+pub fn window_values(digits: Vec<u64>, span: usize) -> Vec<u64>{
+    let mut window_values = digits
+        .windows(span)
+        .map(|w| w.to_vec())
+        .map(|v| v.iter().fold(1, |acc, i| acc * i))
+        .collect::<Vec<u64>>();
+    window_values.sort();
+    window_values.reverse();
+    window_values
+} 
